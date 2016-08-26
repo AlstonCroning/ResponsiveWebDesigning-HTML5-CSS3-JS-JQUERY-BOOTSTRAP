d93808c58a71971a3314bfca1cdb162b26e6bdf8
Alston <and_croning@hotmail.com>
2016-08-26 14:34:41 -0400

    jquery and css done for bxslider section

--- BuildingRealResponsiveWebsite/js/javascript-custom.js
+++ BuildingRealResponsiveWebsite/js/javascript-custom.js
@@ -128,3 +128,16 @@ $(document).ready(function () {
     }, 10);
 
 });
+
+//add bx slider to screens
+$(document).ready(function () {
+
+    $('.bxslider').bxSlider({
+        slideWidth: 292.5,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 3,
+        slideMargin: 50
+    });
+
+});
