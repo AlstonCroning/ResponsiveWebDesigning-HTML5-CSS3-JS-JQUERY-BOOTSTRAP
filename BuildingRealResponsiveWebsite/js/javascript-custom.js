5c18a3649cf0baf556bdb48477315a09facf7d8b
Alston <and_croning@hotmail.com>
2016-08-26 00:12:52 -0400

    more jquery effects added for navbar and completed section navbar

--- BuildingRealResponsiveWebsite/js/javascript-custom.js
+++ BuildingRealResponsiveWebsite/js/javascript-custom.js
@@ -60,3 +60,47 @@ $(document).ready(function () {
         }
     });
 });
+
+//active menu item onclick
+$(document).ready(function () {
+    'use strict';
+
+    $('.navbar-nav li a').click(function () {
+        'use strict';
+
+        $('.navbar-nav li a').parent().removeClass("active");
+
+        $(this).parent().addClass("active");
+
+    });
+
+});
+
+//highlight menu item on scroll
+$(document).ready(function () {
+
+    'use strict';
+
+    $(window).scroll(function () {
+
+        'use strict';
+
+        $("section").each(function () {
+
+            'use strict';
+
+            var bb = $(this).attr("id"); //ABOUT, HOME, CONTACT, DOWNLOAD
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+
+            if( $(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei ) {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+            } else {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+            }
+
+        });
+
+    });
+
+});
